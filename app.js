bca2da72a0eabb162f9f2bd9417727e562e09fdf
shabana-qaiser <shabanaqaiser333@gmail.com>
2020-06-16 01:38:19 +0500

    now ready to commit

---
+++ app.js
@@ -0,0 +1,311 @@
+// chapter # 1 task # 1
+// alert("Hello World")
+
+// chapter # 1 task # 2
+// alert("Error! please enter a valid password ")
+
+// chapter # 1 task # 3
+// alert("welcome  to js land... \n Happy coding!")
+
+// chapter # 1 task # 4
+// alert("welcome to js land...")
+// alert("Happy coding!")
+
+// chapter # 1 task # 5
+// alert("Hello! I can run js through my web browser console")
+
+
+// chapter #2 Q # 1
+// var username
+// username = ("type your name");
+// alert("username");
+
+// chapter #2 Q # 2
+
+// var x ="my name"
+// alert(x);
+// var y="noor"
+// alert(y);
+
+// var arr = ["rania","fatmaa"]
+// var lastname = ["rayan", "rehman"];
+// for(var i = 0; i < arr.length; i++){
+//     for(var j = 0; j < lastname.length;j++){
+//     alert(arr[i] + " " + lastname[j])
+//     }
+// }
+
+// chapter #2 Q # 3
+
+// var t ="Hello World"
+// const(t);
+
+// document.write("Hello World");
+
+
+// chapter #2 Q # 4
+// alert("jhone doe")
+// alert("15 years old")
+// alert("certified mobile application development")
+
+// chapter #2 Q # 5
+// var u =("PIZZA \n" )
+// var  v=("PIZZ \n" )
+// var w =("PIZ \n" )
+// var x =("PI \n" )
+// var y =("P" )
+// var z =(u + v + w + x + y);
+// alert(z);
+
+// chapter #2 Q # 6
+// var a =(" my email address is example@")
+// var b =("example.com")
+// var c =(a + b)
+// alert(c);
+
+
+// chapter #2 Q # 7
+// var book =("I am trying to learn from the Book A smarter \n way to learn JavaScript" )
+// alert(book);
+
+// chapter #2 Q # 8
+// document.write("Yah! I can write HTML content through JavaScript")
+// chapter #2 Q # 9
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+// chapter # 3 Q # 1
+// var a = prompt("Enter your age","your age is;" );
+// var b =("I am" )
+// var c =( "years  old")
+// var d =(b + a + c)
+// alert(d);
+
+// chapter # 3 Q # 2
+
+// ...???
+
+// chapter # 3 Q # 3
+
+// var a =prompt("birthYear;");
+// document.write ("my birth year is" + a,"<br>")
+// document .write ("Data type of my declared variable is number")
+
+// chapter # 3 Q # 4
+
+// var Visitor =prompt("visitor name", "Enter your name")
+// var Visitor =prompt("Product title", "your order")
+// var Visitor =prompt("Quantity", "ordered type")
+// document.write("<b> Jhon Doe </b>","ordered","<b> 5 T-shirt</b>","(s) on XYZ Clothing store")
+
+// chapter # 5 Q # 1
+
+// var a =3;
+// var b =5;
+// var c =a+b;
+// document.write("<b>","sum of 3 and 5 is 8","</b>");
+
+// chapter # 5 Q # 2
+
+// var a =3;
+// var b =5;
+// var c =a-b;
+// document.write("<b>","subtaction of 3 and 5 is -2","</b>");
+
+// var a =3;
+// var b =5;
+// var c =a*b;
+// document.write("<b>","multipication of 3 and 5 is 15","</b>");
+
+// var a =3;
+// var b =5;
+// var c =a/b;
+// document.write("<b>","division of 3 and 5 is 0.6","</b>");
+
+// chap# 5 q # 2
+
+
+// chap# 5 q # 3
+
+// var number;
+// document.write("value after variable decleration is" + " " + number + "<br>")
+// number =5;
+// document.write("initial value;" + " " + number + "<br>")
+// number++
+// document.write("value after increment is" + " " + number + "<br>")
+// number = number + 7
+// document.write("value after addition  is" + " " + number + "<br>")
+// number --
+// document.write("value after decrement is" + " " + number + "<br>")
+// number = number % 3
+// document.write("value after reminder is" + " " + number + "<br>")
+
+
+// chap# 5 q # 4
+// "<script>"
+// var a  =600;
+// var b  =5;
+// var c = (a * b)
+// console.log(c);
+// document.write("<h2>total cost to buy 5 tickets to a movie is 3000 PKR ")
+// "</script>"
+
+// chap# 5 q # 8
+
+// var totalmarks= 980;
+// var marksobtained= 804;
+// var per =(marksobtained * 100)/ totalmarks
+// document.write("<h1>Marks Sheet</h1>")
+// document.write("total marks:"+ " " +totalmarks +"</br> marksobtained:"+" " 
+// +marksobtained+ "<br> percentage:" + "" + per + "%" )
+
+// chap# 5 q # 9
+// document.write("<h1> Currency in PKR </h1>");
+// var us$
+// us$ =104.80;
+// var saudi
+// saudi  = 28;
+// var Pakistani =("us$ + saudi")
+// document.write=( "Pakistani");
+
+// chapter # 6  Q # 6 a
+
+// var English = prompt("Enter marks you obtained in English");
+// var Math = prompt("Enter marks you obtained in Math");
+// var Urdu = prompt("Enter marks you obtained in Urdu");
+// var subtotal =100;
+// var totalmarks = 100;
+// var Englishper = ("English/subtotal")*100;
+// var Mathper = ("Math/subtotal")*100;
+// var Urduper = ("Urdu/subtotal")*100;
+// var totalnumber = ("totalobtained/total")*100;
+// document.write("<tabla>","<tr>","<td>","Subtotal","</td>","nbps","<td>","totalmarks","</td>","nbps",
+// "<td>","obtainedmarks","</td>","nbps","<td>percentage", "</td>","nbps","</tr>"</tabla>");
+ 
+// chap # 9 q # 1
+
+// var a= "A"
+// undefined
+// a.charCodeAt()
+
+// chapter 12 to 13 Q # 1
+ 
+// var char = prompt("Enter your string");
+// if (char >= 65 && char <= 90) {
+//     alert("your chararacter is upercase")
+// }
+// else if (char >=97 && char <=122){
+//     alert("your chararacter is lowercase")
+// }
+
+// chapter 12 to 13 Q # 2
+
+// var a = +prompt("Enter  your  first  number ")
+// var b = +prompt("Enter  your  second  number ")
+// if(a == b){
+//     alert("both integers are equel")
+// }else if (a > b){
+//     alert("first integer is greater than second  integer") 
+// }else{
+//     alert("second  integer is greater than first integer")
+// }
+
+
+// chapter # 12 to 13 Q # 5
+ 
+// var a =prompt("Enter your password ")
+// var a =("newyear1"); 
+// var b =prompt("Enter your password ")
+// var b =("newyear1"); 
+// if(a =b){
+//     alert("correct !");
+// }else if (a,b = " "){
+//     alert("Enter your password  first")
+// }else{
+//     ("your password  is not correct ")
+// }
+
+// chapter # 12 to 13 Q # 5
+
+// var password 
+// password =prompt("Enter your password ")
+// var password =("newyear")
+// if(password == "newyear"){
+//     alert("correct!" )
+// }else{
+//     alert("password  is not correct" )
+// }
+
+
+// chapter # 12 to 13 Q # 6
+
+// var greeting =prompt("enter time");
+// var hour = 13;
+// if(hour < "13"){ 
+//     alert( "Good day")
+// }else(hour > "13")
+// {
+//     alert( "Good evening"); 
+// }
+
+// chapter # 12 to 13 Q # 7
+
+// var time =prompt("enter time");
+// var time = "19:00"
+// if(time >= "00:00 && < 17:00")
+// { 
+//     alert( "Good morning")
+// }else(time >= "12:00 && <17:00")
+// {
+//     alert( "Good afternoon"); 
+// }if(time >= "17:00 && < 21:00")
+// { 
+//     alert( "Good evening")
+// }else(time >= "21:00 && <23:59")
+// {
+//     alert( "Good night"); 
+// }
+
+// chp #14 to 16 q # 1
+
+// var student =[];
+// student [0] = ("nadia");
+// alert ("student")
+
+// chp #14 to 16 q # 2
+
+// var a =[];
+// var name = nadia();
+// alert(name);
+
+// chp #14 to 16 q # 8
+
+// var arr =["Michael","Jhon","Tony"];
+// var arr =["320","230","480"];
+// document.write("<h2>Score of Michael is 320. Percentage: 64% </h2>")
+// document.write("<h2>Score of Michael is 230. Percentage: 46% </h2>")
+// document.write("<h2>Score of Michael is 480. Percentage: 96% </h2>")
+
+// chp #14 to 16 q # 15
+
+// KeyboardEvent
+// var arr = ["Nokia","Samsung","Iphone"]
+
+// document.write("<select> ")
+// for(var i = 0; i < arr.length; i++){
+//     document.write("<option>" + arr[i] + " </option>")
+// }
+// document.write("<select>")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
